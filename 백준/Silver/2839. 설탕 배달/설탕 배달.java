48f6a43ef810b450341a0b308c659eaef8770ff2
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2022-10-20 20:14:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+       
+		if(n == 4 || n == 7) {
+			bw.write(-1 + "");
+		}else if(n % 5 == 0) {
+			bw.write(n / 5 + "");
+		}else if((n % 5) == 1 || (n % 5) == 3) {
+			bw.write((n / 5) + 1 + "");
+		}else{
+			bw.write((n / 5) + 2 + "");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
