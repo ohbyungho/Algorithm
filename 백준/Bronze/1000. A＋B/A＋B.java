48e826a79e720cf1794298b4fe1eb5efc10720ef
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2022-10-20 19:24:57 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		int a, b;
+		a = num.nextInt();
+		b = num.nextInt();
+		System.out.println(a + b);
+	}
+}
