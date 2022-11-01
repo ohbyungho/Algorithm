1fc172cce664c9d6234a2c3713b6bf7e70b5eacd
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2022-11-01 18:45:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 904 ms, Memory: 63072 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		Integer[][] p = new Integer[n][2];
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			p[i][0] = Integer.parseInt(st.nextToken());
+			p[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(p, new Comparator<Integer[]>() {
+
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				if(o1[0].equals(o2[0])) {
+					return o1[1] - o2[1];
+				}else {
+					return o1[0] - o2[0];
+				}
+			}
+		});
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(Integer[] m : p) {
+			bw.write(m[0] + " " + m[1] + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
