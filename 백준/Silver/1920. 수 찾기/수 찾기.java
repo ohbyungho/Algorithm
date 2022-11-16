2fa692ad378005a9a46db6a80813e86fcdfbe2d9
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2022-11-16 16:21:36 +0900

    [Silver IV] Title: 수 찾기, Time: 580 ms, Memory: 49696 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
@@ -8,6 +10,7 @@ public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
 		HashSet<Integer> set = new HashSet<>();
@@ -21,10 +24,13 @@ public class Main {
 		for(int i = 0; i < m; i++) {
 			int num = Integer.parseInt(st.nextToken());
 			if(set.contains(num)) {
-				System.out.println(1);
+				bw.write(1 + "\n");
 			}else {
-				System.out.println(0);
+				bw.write(0 + "\n");
 			}
 		}
+		bw.flush();
+		bw.close();
+		br.close();
     }
 }
