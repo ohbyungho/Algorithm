0be431cfd43231930c05e3cf75be5cf9a7470188
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2025-03-28 15:36:11 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 18008 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+
+	        int K = Integer.parseInt(st.nextToken());
+	        int N = Integer.parseInt(st.nextToken());
+	        long max=0;
+	        long mid=0;
+	        long min=0;
+	        int arr[] = new int[K];
+	        for (int i = 0; i < K; i++) {
+	        	st = new StringTokenizer(br.readLine());
+	            int number = Integer.parseInt(st.nextToken());
+	            arr[i] = number;
+	            if(number>max){
+	                max = number;
+	            }
+	        }
+	        max++;
+	        long n;
+	        while(min<max){
+	            n=0;
+	            mid = (min + max) / 2;
+	            for(int i=0; i<K; i++){
+	                n += (arr[i]/mid);
+	            }
+
+	            if(n<N){
+	                max = mid;
+	            }
+	            else{
+	                min = mid +1;
+	            }
+	        }
+	        System.out.print(min-1);
+	}
+
+}
