c9b4d33686a8d7e40dd35b07846212c1b5493393
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2025-04-01 12:10:14 +0900

    Update 폰켓몬.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -5,27 +5,30 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         int[] arr = Arrays.stream(nums).distinct().toArray();
-                    
+               
         return Math.min(arr.length, nums.length / 2);
+        
+        /** 풀이 1
+            if(arr.length > nums.length / 2){ 
+                answer = nums.length / 2;
+            }else{ 
+                answer = arr.length;
+             }
+
+             return answer;                             
+        */ 
+
+        /** 풀이 2        
+            HashSet<Integer> kinds = new HashSet<>();
+
+            for (int num : nums) {
+                kinds.add(num);
+            }
+
+            int maxPick = nums.length / 2;
+
+            return Math.min(kinds.size(), maxPick);
+        */
         
     }
 }
-
-// 2번 풀이
-//import java.util.HashSet;
-
-//class Solution {
-//    public int solution(int[] nums) {
-        
-//        HashSet<Integer> kinds = new HashSet<>();
-        
-//        for (int num : nums) {
-//            kinds.add(num);
-//        }
-
-//        int maxPick = nums.length / 2;
-
-//        return Math.min(kinds.size(), maxPick);
-//    }
-//}
-
