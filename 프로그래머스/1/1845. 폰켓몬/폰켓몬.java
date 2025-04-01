d1847ba2f24c86597abed9f327b7cf7c3f2d2da7
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2025-04-01 12:04:05 +0900

    [level 1] Title: 폰켓몬, Time: 12.98 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,35 +1,31 @@
 // 1번 풀이
-//import java.util.Arrays;
-
-//class Solution {
-//    public int solution(int[] nums) {
-//        int answer = 0;
-//        int[] arr = Arrays.stream(nums).distinct().toArray();
-//        
-//        if(arr.length > nums.length / 2){
-//            answer = nums.length / 2;
-//        }else{ 
-//            answer = arr.length;
-//        }
-//        
-//        return answer;
-//    }
-//}
-
-// 2번 풀이
-import java.util.HashSet;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] nums) {
+        int answer = 0;
+        int[] arr = Arrays.stream(nums).distinct().toArray();
+                    
+        return Math.min(arr.length, nums.length / 2);
         
-        HashSet<Integer> kinds = new HashSet<>();
-        
-        for (int num : nums) {
-            kinds.add(num);
-        }
-
-        int maxPick = nums.length / 2;
-
-        return Math.min(kinds.size(), maxPick);
     }
 }
+
+// 2번 풀이
+//import java.util.HashSet;
+
+//class Solution {
+//    public int solution(int[] nums) {
+        
+//        HashSet<Integer> kinds = new HashSet<>();
+        
+//        for (int num : nums) {
+//            kinds.add(num);
+//        }
+
+//        int maxPick = nums.length / 2;
+
+//        return Math.min(kinds.size(), maxPick);
+//    }
+//}
+
