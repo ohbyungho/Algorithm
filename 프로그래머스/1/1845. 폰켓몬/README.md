8e61dc7a1308072309ac19f56599f109f6ef583d
Ohbottle <101032093+ohbyungho@users.noreply.github.com>
2025-04-01 11:53:34 +0900

    [level 1] Title: 폰켓몬, Time: 6.44 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 8.67 ms
+메모리: 91.8 MB, 시간: 6.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 01일 11:43:18
+2025년 04월 01일 11:53:31
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,35 @@
-import java.util.Arrays;
+// 1번 풀이
+//import java.util.Arrays;
+
+//class Solution {
+//    public int solution(int[] nums) {
+//        int answer = 0;
+//        int[] arr = Arrays.stream(nums).distinct().toArray();
+//        
+//        if(arr.length > nums.length / 2){
+//            answer = nums.length / 2;
+//        }else{ 
+//            answer = arr.length;
+//        }
+//        
+//        return answer;
+//    }
+//}
+
+// 2번 풀이
+import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int[] arr = Arrays.stream(nums).distinct().toArray();
         
-        if(arr.length > nums.length / 2){
-            answer = nums.length / 2;
-        }else{ 
-            answer = arr.length;
+        HashSet<Integer> kinds = new HashSet<>();
+        
+        for (int num : nums) {
+            kinds.add(num);
         }
-        
-        return answer;
+
+        int maxPick = nums.length / 2;
+
+        return Math.min(kinds.size(), maxPick);
     }
 }
